3db042710e144076f9074fcdeb39fa8ae4b9b09d
zyzzchad <119152524+gautamalish@users.noreply.github.com>
2024-03-17 22:40:32 +0545

    title and navigation made

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 
 import './App.css'
-
+import FrontPage from './FrontPage'
 function App() {
 
   return (
-    <div>
-      <h1>Hello React</h1>
+    <div className='mainContainer'>
+      <FrontPage/>
     </div>
   )
 }
---
+++ src/FrontPage.jsx
@@ -0,0 +1,23 @@
+import hostel from "./images/hostel.jpg"
+export default function FrontPage(){
+    return(
+        <div className="frontPageContainer">
+        <div className="logo">
+            <img src={hostel} alt="hostel" className="logoImg"/>
+            <div className="title">
+                <h1>HostelStays</h1>
+                <p>Kathmandu,Nepal</p>
+            </div>
+        </div>
+        <div className="nav">
+            <ul className="navItems">
+                <li>Home</li>
+                <li>About Us</li>
+                <li>Services</li>
+                <li>Policy</li>
+                <li>Contact Us</li>
+            </ul>
+        </div>
+        </div>
+    )
+}
