fd06392a8571badd983b1d3998d425478336e90e
chaudharyaashish <np03cs4a220166@heraldcollege.edu.np>
2024-04-09 22:44:25 +0545

    Add chart components and update files

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,19 +1,24 @@
-import React from 'react'
-import hostel from "../../images/hostel.jpg"
+import React from "react";
+import hostel from "../../images/hostel.jpg";
 import { IoNotifications } from "react-icons/io5";
-import "./navbar.scss"
-import { NavLink } from 'react-bootstrap';
+import "./navbar.scss";
+import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
+import LanguageOutlinedIcon from "@mui/icons-material/LanguageOutlined";
+
+import FullscreenExitOutlinedIcon from "@mui/icons-material/FullscreenExitOutlined";
+import NotificationsNoneOutlinedIcon from "@mui/icons-material/NotificationsNoneOutlined";
+import ChatBubbleOutlineOutlinedIcon from "@mui/icons-material/ChatBubbleOutlineOutlined";
+
 function Navbar() {
   return (
-    <div className='navContainer'>
-      <div className="left">
-      </div>
+    <div className="navContainer">
+      <div className="left"></div>
       <div className="right">
-      <IoNotifications className='bellIcon'/>
-      <img src={hostel} className='hostelImg' alt="Hostel logo" />
+        <IoNotifications className="bellIcon" />
+        <img src={hostel} className="hostelImg" alt="Hostel logo" />
       </div>
     </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
--- src/components/widgets/Widgets.jsx
+++ src/components/widgets/Widgets.jsx
@@ -1,11 +1,100 @@
-import React from 'react'
+import React from "react";
+import "./widgets.scss";
+import KeyboardArrowUpOutlinedIcon from "@mui/icons-material/KeyboardArrowUpOutlined";
+import PersonOutlineOutlinedIcon from "@mui/icons-material/PersonOutlineOutlined";
+import MonetizationOnOutlinedIcon from "@mui/icons-material/MonetizationOnOutlined";
+import AccountBalanceOutlinedIcon from "@mui/icons-material/AccountBalanceOutlined";
+import BedroomChildOutlinedIcon from "@mui/icons-material/BedroomChildOutlined";
 
-function Widgets() {
+function Widget({ type }) {
+  let data;
+  let amount; // Define amount variable
+
+  const diff = 20;
+
+  switch (type) {
+    case "user":
+      data = {
+        title: "Total Students",
+        isMoney: false,
+        link: "See all Students",
+        icon: (
+          <PersonOutlineOutlinedIcon
+            className="icon"
+            style={{ color: "crimson", backgroundColor: "rgba(255,0,0,0.2)" }}
+          />
+        ),
+      };
+      amount = 35;
+      break;
+    case "balance":
+      data = {
+        title: "Total Paid students",
+        isMoney: false,
+        link: "See details",
+        icon: (
+          <AccountBalanceOutlinedIcon
+            className="icon"
+            style={{ color: "purple", backgroundColor: "rgba(128,0,128,0.2)" }}
+          />
+        ),
+      };
+      amount = 20;
+      break;
+    case "order":
+      data = {
+        title: "Total Rooms",
+        isMoney: false,
+        link: "View all Rooms",
+        icon: (
+          <BedroomChildOutlinedIcon
+            className="icon"
+            style={{
+              color: "goldenrod",
+              backgroundColor: "rgba(218,165,32,0.2)",
+            }}
+          />
+        ),
+      };
+      amount = 25;
+      break;
+    case "earning":
+      data = {
+        title: "Available Rooms",
+        isMoney: false,
+        link: "View Available Rooms",
+        icon: (
+          <BedroomChildOutlinedIcon
+            className="icon"
+            style={{ color: "green", backgroundColor: "rgba(0,128,0,0.2)" }}
+          />
+        ),
+      };
+      amount = 10;
+      break;
+    default:
+      break;
+  }
+
   return (
-    <div>
-      
+    <div className="widget">
+      <div className="left">
+        <span className="title">{data.title}</span>
+        <span className="counter">
+          {data.isMoney && "$"}
+          {amount} {/* Use the amount variable here */}
+        </span>
+        <span className="link">{data.link}</span>
+      </div>
+      <div className="right">
+        <div className="percentage positive">
+          <KeyboardArrowUpOutlinedIcon />
+          {`${diff}%`}
+        </div>
+        {data.icon}
+      </div>
     </div>
-  )
+  );
 }
 
-export default Widgets
+export default Widget;
