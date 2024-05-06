fb325154cbd7b78723ff50ead7fd48b92e12f88b
Savona Shrestha <np03cs4a220177@heraldcollege.edu.np>
2024-05-06 21:50:16 +0545

    Added Popup Menu button in profile with edit profile, settings and logout

--- FrontEndContents/src/components/navbar/Navbar.jsx
+++ FrontEndContents/src/components/navbar/Navbar.jsx
@@ -1,15 +1,45 @@
-import React from 'react';
+import React, { useState, useRef, useEffect} from 'react';
 import hostel from "../../images/hostel.jpg"
+import profile from "../../images/profile.png"
+import settings from "../../images/settings.png"
+import logout from "../../images/logout.png"
 import { IoNotifications } from "react-icons/io5";
 import "./navbar.scss"
+import { NavLink } from 'react-bootstrap';
 function Navbar() {
+  const [showPopover, setShowPopover] = useState(false);
+
+  const imgRef = useRef(null);
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (imgRef.current && !imgRef.current.contains(event.target)) {
+        setShowPopover(false);
+      }
+    };
+
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [imgRef]);
   return (
     <div className='navContainer'>
       <div className="left">
       </div>
       <div className="right">
       <IoNotifications className='bellIcon'/>
-      <img src={hostel} className='hostelImg' alt="Hostel logo" />
+      <img src={hostel} className='hostelImg' alt="Hostel logo" onClick={() => setShowPopover(!showPopover)} />
+      {showPopover && (
+            <div className="popover">
+              <div className="popover-item">
+              <img src={profile} className='EditImg' alt="Profile" ></img>Edit Profile</div>
+              <div className="popover-item">
+              <img src={settings} className='SettImg' alt="settings"></img>Settings</div>
+              <div className="popover-item">
+              <img src={logout} className='logoutImg' alt="logout"></img>Log Out</div>
+            </div>
+          )}
       </div>
     </div>
   );
