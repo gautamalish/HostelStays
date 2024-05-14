fd1231a8db51d5528fc854cd7147be9d6aa37c01
Savona Shrestha <np03cs4a220177@heraldcollege.edu.np>
2024-05-14 19:50:18 +0545

    Added Contents in the Profile Page

--- FrontEndContents/src/pages/Uprofile/Uprofile.jsx
+++ FrontEndContents/src/pages/Uprofile/Uprofile.jsx
@@ -4,7 +4,6 @@ import Navbar from "../../components/navbar/Navbar";
 import "./Uprofile.scss";
 
 function Uprofile() {
- 
   return (
     <div className="uprofile">
       <Sidebar />
@@ -12,6 +11,30 @@ function Uprofile() {
         <Navbar />
         <div className="uprofileContents">
           <h1>Profile</h1>
+          <div className="profile">
+            <img src="" className="profileImg" alt="Profile icon"/>
+          </div>
+          <div className='box'>
+            <label htmlFor="roomImage">Full Name: </label>
+            <input type="text" placeholder='Fullname' />
+          </div>
+          <div className='box'>
+            <label htmlFor="roomImage">Username: </label>
+            <input type="text" placeholder='Username' />
+          </div>
+          <div className='box'>
+            <label htmlFor="roomImage">Email: </label>
+            <input className='box-email' type="text" placeholder='Email' />
+            <p>This is your primary email address.</p>
+          </div>
+          <div className='box'>
+          <label htmlFor="roomImage">Address: </label>
+            <input className='box-address' type="text" placeholder='Address' />
+          </div>
+          <div className='box'>
+            <label htmlFor="roomImage">Phone Number: </label>
+            <input type="text" placeholder='Phone Number' />
+          </div>
         </div>
       </div>
     </div>
