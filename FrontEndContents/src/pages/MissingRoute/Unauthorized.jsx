60f558d96bf0e94233965a6934ae4a655c2ba49f
zyzzchad <119152524+gautamalish@users.noreply.github.com>
2024-05-15 14:17:30 +0545

    used useNavigate to navigate back from the missing page

--- FrontEndContents/src/pages/MissingRoute/Unauthorized.jsx
+++ FrontEndContents/src/pages/MissingRoute/Unauthorized.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import "./unauthorized.scss"
 import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 function Unauthorized() {
+  const navigate=useNavigate()
     return (
         <div className="not-found">
           <h1>404 - Page Not Found</h1>
           <p>The page you are looking for does not exist.</p>
-          <Link to="/">Go to Login</Link>
+          <p onClick={()=>navigate(-1)} className='back'>Go back</p>
         </div>
       );
 }
